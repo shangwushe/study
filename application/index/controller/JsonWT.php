7cde953df1dec2b6b06becd812392453dfae49da
shangwushe <shangwushe@gmail.com>
2019-07-04 19:49:12 +0800

    jwt

---
+++ application/index/controller/JsonWT.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019-07-04
+ * Time: 14:14
+ */
+namespace app\index\controller;
+
+class JsonWT extends \think\Controller{
+
+}
