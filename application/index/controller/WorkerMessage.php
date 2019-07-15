22dab6e3337429b5a67980276a15c7c4dc97059c
shangwushe <shangwushe@gmail.com>
2019-07-15 23:34:16 +0800

    work message

--- application/index/controller/WorkerMessage.php
+++ application/index/controller/WorkerMessage.php
@@ -120,6 +120,64 @@ class WorkerMessage extends Controller {
         /***********param onWorkerReload***********/
         //设置Worker收到reload信号后执行的回调。可以利用onWorkerReload回调做很多事情，例如在不需要重启进程的情况下重新加载业务配置文件。
 
+        /***********param onConnect***********/
+        //当客户端与Workerman建立连接时(TCP三次握手完成后)触发的回调函数。每个连接只会触发一次onConnect回调。
+        //注意：onConnect事件仅仅代表客户端与Workerman完成了TCP三次握手，这时客户端还没有发来任何数据，此时除了通过$connection->getRemoteIp()获得对方ip，没有其他可以鉴别客户端的数据或者信息，所以在onConnect事件里无法确认对方是谁。要想知道对方是谁，需要客户端发送鉴权数据，例如某个token或者用户名密码之类，在onMessage回调里做鉴权。
+        /*$worker->onConnect = function($connection)
+        {
+            echo "new connection from ip " . $connection->getRemoteIp() . "\n";
+        };*/
+
+        /***********param onMessage***********/
+        //当客户端通过连接发来数据时(Workerman收到数据时)触发的回调函数
+        /*$worker->onMessage = function($connection, $data)
+        {
+            var_dump($data);
+            $connection->send('receive success');
+        };*/
+
+        /***********param onClose***********/
+        //当客户端连接与Workerman断开时触发的回调函数。不管连接是如何断开的，只要断开就会触发onClose。每个连接只会触发一次onClose。
+        //注意：如果对端是由于断网或者断电等极端情况断开的连接，这时由于无法及时发送tcp的fin包给workerman，workerman就无法得知连接已经断开，也就无法及时触发onClose。这种情况需要通过应用层心跳来解决。workerman中连接的心跳实现参见这里。如果使用的是GatewayWorker框架，则直接使用GatewayWorker框架的心跳机制即可，参见这里。
+        /*$worker->onClose = function($connection)
+        {
+            echo "connection closed\n";
+        };*/
+
+        /***********param onBufferFull***********/
+        /*$worker->onBufferFull = function($connection)
+        {
+            echo "bufferFull and do not send again\n";
+        };*/
+
+        /***********param onBufferDrain***********/
+        //每个连接都有一个单独的应用层发送缓冲区，缓冲区大小由TcpConnection::$maxSendBufferSize决定，默认值为1MB，可以手动设置更改大小，更改后会对所有连接生效。
+        //该回调在应用层发送缓冲区数据全部发送完毕后触发。一般与onBufferFull配合使用，例如在onBufferFull时停止向对端继续send数据，在onBufferDrain恢复写入数据。
+        /*$worker->onBufferDrain = function($connection)
+        {
+            echo "buffer drain and continue send\n";
+        };*/
+
+        /***********param onError***********/
+        //当客户端的连接上发生错误时触发。
+        //目前错误类型有
+        //1、调用Connection::send由于客户端连接断开导致的失败（紧接着会触发onClose回调） (code:WORKERMAN_SEND_FAIL msg:client closed)
+        //2、在触发onBufferFull后(发送缓冲区已满)，仍然调用Connection::send，并且发送缓冲区仍然是满的状态导致发送失败(不会触发onClose回调)(code:WORKERMAN_SEND_FAIL msg:send buffer full and drop package)
+        //3、使用AsyncTcpConnection异步连接失败时(紧接着会触发onClose回调) (code:WORKERMAN_CONNECT_FAIL msg:stream_socket_client返回的错误消息)
+        /*$worker->onError = function($connection, $code, $msg)
+        {
+            echo "error $code $msg\n";
+        };*/
+
+        /***********param runAll***********/
+        //运行所有Worker实例。
+        //Worker::runAll()执行后将永久阻塞，也就是说位于Worker::runAll()后面的代码将不会被执行。所有Worker实例化应该都在Worker::runAll()前进行。
+        //注意：windows版本的workerman不支持在同一个文件中实例化多个Worker。 上面的例子无法在windows版本的workerman下运行。windows版本的workerman需要将多个Worker实例初始化放在不同的文件中
+
+        /***********param stopAll***********/
+        //停止当前进程（子进程）的所有Worker实例并退出。此方法用于安全退出当前子进程，作用相当于调用exit/die退出当前子进程。与直接调用exit/die区别是，直接调用exit或者die无法触发onWorkerStop回调，并且会导致一条WORKER EXIT UNEXPECTED错误日志。
+
+        /***********param param_name***********/
         /***********param param_name***********/
 
         // 接收到浏览器发送的数据时回复hello world给浏览器
